d8b99632fea6e3deadc60ade0c917389ce175b6b
kriti009 <kriti.ritu.dew@gmail.com>
2019-01-30 17:56:07 +0530

    add header and footer partials

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var request = require('request');
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
 
 var url1 = "https://pokeapi.co/api/v2/pokemon/";
 app.get("/", function(req, res){
